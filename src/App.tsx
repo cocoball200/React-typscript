6a7725cc09f05ae181dbcbdb84b19ce3ebd983e9
mia <cocoball200@gmail.com>
2021-01-09 19:12:00 +0900

    how to use state in react with typescript

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Component } from 'react';
+
+interface IState {
+  counter: number;
+}
+class App extends Component<{}, IState>{
+  state = {
+    counter: 0
+  };
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        {counter} <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+
+  add = () => {
+    this.setState(prev => {
+      return {
+        counter: prev.counter + 1
+      }
+    })
+  }
+
+}
+
+export default App;
